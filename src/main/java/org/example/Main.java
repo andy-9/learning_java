e20b4bcd7a5124a34933a0d71cdbf56297b7bf55
Andreas Hechler <info@andreashechler.com>
2023-12-10 23:09:50 +0100

    minor changes in code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Eingabe with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!\n");
 
         // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i < 5; i++) {
 
             // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Strg+F8.
