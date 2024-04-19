325a6e1a5b7f455eb1cc38d863ea4b72536d5274
Andreas Hechler <andreas.hechler@partner.bmw.de>
2024-04-19 18:30:10 +0200

    GuestList, pt. 1

--- src/main/java/java_for_absolute_beginners/sec_11_guest_list/GuestList.java
+++ src/main/java/java_for_absolute_beginners/sec_11_guest_list/GuestList.java
@@ -8,8 +8,14 @@ public class GuestList {
         String[] guests = new String[10];
         Scanner scanner = new Scanner(System.in);
 
+        guests[0] = "Jacob";
+        guests[1] = "Edward";
+        guests[2] = "Rose";
+        guests[3] = "Molly";
+        guests[4] = "Christopher";
+
         do {
-            System.out.println();
+            System.out.println("\n__________________________________\n- Menu -\n");
             System.out.println("1 - Display all Guests");
             System.out.println("2 - Add Guests");
             System.out.println("3 - Remove Guests");
@@ -19,14 +25,24 @@ public class GuestList {
             System.out.println();
 
             if (option == 1) {
+                System.out.println("\n__________________________________\n- Guests -\n");
                 for (final String guest : guests) {
-                    System.out.println(guest);
+                    System.out.println(guest == null ? "--" : guest);
                 }
             } else if (option == 2) {
                 for (int i = 0; i < guests.length; i++) {
                     if (guests[i] == null) {
                         System.out.print("Name: ");
                         guests[i] = scanner.next();
+                        break;
+                    }
+                }
+            } else if (option == 3) {
+                System.out.print("Name: ");
+                String name = scanner.next();
+                for (int i = 0; i < guests.length; i++) {
+                    if (guests[i] != null && guests[i].equals(name)) {
+                        guests[i] = null;
                         break;
                     }
                 }
